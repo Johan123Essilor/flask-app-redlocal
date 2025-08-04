000f9148b41668e84cb91303dba8e54370609fe4
Johan123Essilor <johan.lozoya@essilorluxottica.id>
2025-08-04 11:37:57 -0700

    puerto 9818

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=9818,debug=False)
 
     # app.run(debug=True)
 
