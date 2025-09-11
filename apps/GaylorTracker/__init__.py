50790828cd93e9f9f488ea5e3cabbf49ee79b8a4
Johan123Essilor <johan.lozoya@essilorluxottica.id>
2025-09-11 17:11:01 -0400

    GaylorTrackerUpdates

--- apps/GaylorTracker/__init__.py
+++ apps/GaylorTracker/__init__.py
@@ -1,34 +1,11 @@
-from flask import Blueprint, render_template, request, session, flash
-from apps.auth.utils import login_required
-from .models import get_all_gaylor
+from flask import Blueprint
 
+# Definir el Blueprint
 gaylorTracker_bp = Blueprint(
     'gaylorTracker', __name__,
     template_folder='templates',
     static_folder='static'
 )
 
-@gaylorTracker_bp.route('/', methods=['GET'])
-# @login_required
-def index():
-    usuario = session.get('usuario')
-    filter_packing = request.args.get('packing_object')
-
-    registros = []
-    total_resultados = 0
-    handling_units_unicas = 0
-
-    # Solo obtener registros si se pasó un filtro
-    if filter_packing:
-        registros = get_all_gaylor(filter_packing=filter_packing)
-        total_resultados = len(registros)
-        handling_units_unicas = len(set(r['handling_unit'] for r in registros)) if registros else 0
-
-    return render_template(
-        'GaylorTrackerIndex.html',
-        registros=registros,
-        usuario=usuario,
-        filter_packing=filter_packing,
-        total_resultados=total_resultados,
-        handling_units_unicas=handling_units_unicas
-    )
+# Importar rutas (para registrarlas en el Blueprint)
+from . import routes
